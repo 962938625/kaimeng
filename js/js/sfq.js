725210537b7d5d30262b7eeed3c2377c027568c0
962938625 <962938625@qq.com>
2019-11-30 14:33:20 +0800

    js综合练习

---
+++ js/js/sfq.js
@@ -0,0 +1,25 @@
+var sfq=document.getElementById('sfq');
+var sfqbox=sfq.getElementsByClassName('sfqbox')[0];
+var ul=sfqbox.getElementsByTagName('ul')[0];
+var lis=ul.getElementsByTagName('li');
+var p=ul.getElementsByTagName('p');
+for(var i=0;i<lis.length;i++){
+    lis[i].index=i;
+    lis[i].onmouseover=function(){
+        n=this.index;
+        for(var i=0;i<lis.length;i++){
+            lis[i].style.width=80+'px';
+        }
+        lis[n].style.width=590+'px';
+        lis[n].style.opacity=1;
+        p[n].style.display='none';
+    }
+    lis[i].onmouseout=function(){
+        for(var i=0;i<lis.length;i++){
+            n=this.index;
+            lis[i].style.width=206+'px';
+        }
+        lis[n].style.opacity=.5;
+        p[n].style.display='block';
+    }
+}
